75f0ee8e1ac441dfa5b618e2e4d370c3e054f3b5
Cameron Clark <cameron.clark31@gmail.com>
2023-03-07 15:04:34 -0500

    test commit

---
+++ Animals.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+
+
+
+{
+    public class Animal
+
+    {
+       
+        public int numberOfLegs { get; set; }
+        public int numberOfEyes { get; set; }
+        public string name { get; set; }
+            
+    }
+
+    public class Cat : Animal
+    {
+
+        public Cat()
+        {
+            this.numberOfLegs = 4;
+            this.numberOfEyes = 2;
+            {
+
+
+
+            }
+
+        }
+    }
+}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Animals;
+
+
+
+
+namespace KnowledgeCheck2
+
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("How many records do you want to add? ");
+            var numberOfRecords = int.Parse(Console.ReadLine());
+
+            var recordList = new List<Cat>();
+            for (int i = 0; i < numberOfRecords; i++)
+            {
+                // In this loop, populate the object's properties using Console.ReadLine()
+                var myClass = new Cat();
+
+                Console.WriteLine("Enter the value for ");
+                myClass. = Console.ReadLine();
+                
+
+
+                recordList.Add(myClass);
+            }
+        }
+
+    }
+}
+
+// Print out the list of records using Console.WriteLine()
